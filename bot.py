8bd5bd917636e2603806f4d4c9b7f52fbd092ac6
Sovenok-Hacker <79601745+Sovenok-Hacker@users.noreply.github.com>
2021-06-04 20:05:58 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,10 @@ from telebot import types
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-                               text = message.text
-                               os.system(text)
+                               if message.text == "Состояние":
+                                                              bot.send_message(message.from_user.id, "Управление сервером запущено! 🔥")
+                               else:
+                                    text = message.text
+                                    os.system(text)
 
 bot.polling()
