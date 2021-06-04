ba175ea781f02f4f9b128cbbcd568a3a07898062
Sovenok-Hacker <79601745+Sovenok-Hacker@users.noreply.github.com>
2021-06-04 20:00:52 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-import telebot, os, random
+import telebot, os
 
 bot = telebot.TeleBot('1802070719:AAGLFlpUBo7CDvHJ1xjoU7icSujXldZuqgM')
 from telebot import types
@@ -6,11 +6,7 @@ from telebot import types
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
-                               bot.send_message(message.from_user.id, "Привет! Я бот которого наисал ползователь Артём Хвостов.")
-                               bot.send_message(message.from_user.id, "Я скоро стану премодератором группы Owl ( сова ) но пока меня не научил этому Артём Хвостов - @Duo_sova . Я уже сейчас являюсь админом этой группы - пока премодератор - Артём. Спасибо за общение со мной!")
                                text = message.text
-                               if text == "Привет":
-                                                   bot.send_message(message.from_user.id, "Привет! Я бот которого наисал ползователь Артём Хвостов.")
-                               print("Получено сообщение - ", text)
+                               os.system(text)
 
 bot.polling()
